d98123cf33aa2928340c20f3c0631ca4922f8902
Mofeyifoluwa David Akomolafe <mofeyifoluwa.akomolafe@mths.ca>
2023-01-09 19:57:45 +0000

    moved code in

---
+++ guess_number.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Feyi Akomolafe All rights reserved
+//
+// Created by: Feyi Akomolafe
+// Created on: Oct 2022
+// This program guess random number
+
+#include <iostream>
+#include <random>
+
+int main() {
+    int randomNumber;
+    int guess;
+
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());
+    std::uniform_int_distribution<int> idist(0, 9);
+    randomNumber = idist(rgen);
+
+    // input
+    std::cout << "Enter the number between 0-9: ";
+    std::cin >> guess;
+    std::cout << std::endl;
+
+    // process & output
+    if (guess == randomNumber) {
+        std::cout << "Congratulations, you guessed correct!" << std::endl;
+    } else {
+        std::cout << "Sorry you guessed wrong, the number was "
+                  << ""
+                  << randomNumber << "" << std::endl;
+    }
+    std::cout << "\nDone.";
+}
